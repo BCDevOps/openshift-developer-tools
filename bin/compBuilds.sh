1dcce079939688efe15c9e3a267909be61114b9e
Wade Barnes <wade.barnes@shaw.ca>
2019-08-09 09:34:50 -0700

    Refactor genBuild process - Align with genDepls process.

--- bin/compBuilds.sh
+++ bin/compBuilds.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
 OCTOOLSBIN=$(dirname $0)
 
+# =================================================================================================================
+# Validation:
+# -----------------------------------------------------------------------------------------------------------------
 _component_name=${1}
 if [ -z "${_component_name}" ]; then
   echo -e \\n"Missing parameter"\\n
   exit 1
 fi
 
+# -----------------------------------------------------------------------------------------------------------------
+# Initialization:
+# -----------------------------------------------------------------------------------------------------------------
 if [ -f ${OCTOOLSBIN}/ocFunctions.inc ]; then
   . ${OCTOOLSBIN}/ocFunctions.inc
 fi
@@ -17,55 +22,77 @@ if [ ! -z "${DEBUG}" ]; then
   set -x
 fi
 
-BUILDS=$(getBuildTemplates $(getTemplateDir))
+# -----------------------------------------------------------------------------------------------------------------
+# Functions:
+# -----------------------------------------------------------------------------------------------------------------
+generateBuildConfigs() {
 
-# Switch to Tools Project
+  BUILDS=$(getBuildTemplates $(getTemplateDir))
+
+  # echo "Build templates:"
+  # for build in ${BUILDS}; do
+  #   echo ${build}
+  # done
+  # exit 1
+
+  for build in ${BUILDS}; do
+    echo -e \\n"Processing build configuration; ${build}..."
+
+    _template="${build}"
+    _template_basename=$(getFilenameWithoutExt ${build})
+    _buildConfig="${_template_basename}${BUILD_CONFIG_SUFFIX}"
+    _searchPath=$(echo $(getDirectory "${_template}") | sed 's~\(^.*/openshift\).*~\1~')
+
+    if [ ! -z "${PROFILE}" ]; then
+      _paramFileName="${_template_basename}.${PROFILE}"
+    else
+      _paramFileName="${_template_basename}"
+    fi
+
+    PARAMFILE=$(find ${_searchPath} -name "${_paramFileName}.param")
+    if [ ! -z "${APPLY_LOCAL_SETTINGS}" ]; then
+      LOCALPARAM=$(find ${_searchPath} -name "${_paramFileName}.local.param")
+    fi
+
+    if [ -f "${PARAMFILE}" ]; then
+      PARAMFILE="--param-file=${PARAMFILE}"
+    else
+      PARAMFILE=""
+    fi
+
+    if [ -f "${LOCALPARAM}" ]; then
+      LOCALPARAM="--param-file=${LOCALPARAM}"
+    else
+      LOCALPARAM=""
+    fi
+
+    oc process --filename=${_template} ${LOCALPARAM} ${PARAMFILE} > ${_buildConfig}
+    exitOnError
+  done
+}
+# =================================================================================================================
+
+# =================================================================================================================
+# Main Script:
+# -----------------------------------------------------------------------------------------------------------------
+# Switch to the Tools project space ...
 switchProject ${TOOLS}
 exitOnError
 
-# Local params file path MUST be relative...Hack!
-LOCAL_PARAM_DIR=${PROJECT_OS_DIR}
+echo -e "Removing dangling configuration files ..."
+cleanBuildConfigs
 
-for build in ${BUILDS}; do
-  echo -e \\n"Processing build configuration; ${build}..."
+echo -e \\n"Generating build configuration files ..."
+generateBuildConfigs
 
-  _template="${build}"
-  _template_basename=$(getFilenameWithoutExt ${build})
-  _buildConfig="${_template_basename}${BUILD_CONFIG_SUFFIX}"
-  _searchPath=$(echo $(getDirectory "${_template}") | sed 's~\(^.*/openshift\).*~\1~')
+if [ -z ${GEN_ONLY} ]; then
+  echo -e \\n"Deploying build configuration files ..."
+  deployBuildConfigs
+fi
 
-  if [ ! -z "${PROFILE}" ]; then
-    _paramFileName="${_template_basename}.${PROFILE}"
-  else
-    _paramFileName="${_template_basename}"
-  fi
-
-  PARAMFILE=$(find ${_searchPath} -name "${_paramFileName}.param")
-  if [ ! -z "${APPLY_LOCAL_SETTINGS}" ]; then
-    LOCALPARAM=$(find ${_searchPath} -name "${_paramFileName}.local.param")
-  fi
-
-  if [ -f "${PARAMFILE}" ]; then
-    PARAMFILE="--param-file=${PARAMFILE}"
-  else
-    PARAMFILE=""
-  fi
-
-  if [ -f "${LOCALPARAM}" ]; then
-    LOCALPARAM="--param-file=${LOCALPARAM}"
-  else
-    LOCALPARAM=""
-  fi
-
-  oc process --filename=${_template} ${LOCALPARAM} ${PARAMFILE} > ${_buildConfig}
-  exitOnError
-  if [ -z ${GEN_ONLY} ]; then
-    oc $(getOcAction) -f ${_buildConfig}
-    exitOnError
-  fi
-
-  # Delete the tempfile if the keep command line option was not specified
-  if [ -z "${KEEPJSON}" ]; then
-    rm ${_buildConfig}
-  fi
-done
+# Delete the configuration files if the keep command line option was not specified.
+if [ -z "${KEEPJSON}" ]; then
+  echo -e \\n"Removing temporary build configuration files ..."
+  cleanBuildConfigs
+fi
+# =================================================================================================================
